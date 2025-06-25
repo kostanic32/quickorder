31fb70c6365a8a393468fdc322e04201ca018753
kostanic32 <kostanicb@gmail.com>
2025-06-25 18:08:59 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -113,4 +113,11 @@ setTimeout(() => {
 
   updateTotal();
 });
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('/service-worker.js').then(function (reg) {
+      console.log('Service worker registered.', reg);
+    });
+  });
+}
 
